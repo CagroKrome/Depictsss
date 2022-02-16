40ac869c9de7ac15394f5fd4eb58f698d83c8f41
ck <45309242+CagroKrome@users.noreply.github.com>
2022-02-16 11:39:21 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function appearHeart() {
 function hideHeart() {
     document.getElementById("kalp").style.opacity = "0%";
 }
+
+function likePost() {
+    document.getElementById("kalp").style.transition = "all 0.4s ease 0s"
+    document.getElementById("kalp").style.opacity = "0%";
+    document.getElementById("kalp").style.width = "160px";
+    document.getElementById("kalp").style.height = "160px";
+}
